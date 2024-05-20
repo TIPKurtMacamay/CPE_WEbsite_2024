735c74faef7b7288df9c63c45bb9c3bc22ceb29e
unknown <marvinganeda2@gmail.com>
2024-05-20 17:36:29 +0800

    fix Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,19 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import logo from '../assets/cpelogo.png';
+import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../assets/cpelogo.png";
 
 const Navbar = () => {
   return (
     <>
-      <div className="bg-green-700 h-1 w-full py-1"></div> {/* Green line on top */}
+      <div className="bg-green-700 h-1 w-full py-1"></div>{" "}
+      {/* Green line on top */}
       <nav className="bg-[#1E1E1E] opacity-100">
         <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
           <div className="relative flex items-center justify-between h-16">
             <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
               {/* Mobile menu button */}
               <button
-                className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+                className="navbarbutton"
                 aria-controls="mobile-menu"
                 aria-expanded="false"
               >
@@ -55,18 +56,60 @@ const Navbar = () => {
               <div className="flex-shrink-0">
                 <img className="h-14 w-15" src={logo} alt="CPE Logo" />
               </div>
-              <h1 className="ml-3 text-white text-m font-bold my-3.5">COMPUTER ENGINEERING</h1>
+              <h1 className="ml-3 text-white text-m font-bold my-3.5">
+                COMPUTER ENGINEERING
+              </h1>
               <div className="hidden sm:block sm:ml-6 my-2">
                 <div className="flex space-x-4">
-                  <Link to="/" className="text-gray-300 hover:bg-gray-600 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Home</Link>
-                  <Link to="/about" className="text-gray-300 hover:bg-gray-600 hover:text-white px-3 py-2 rounded-md text-sm font-medium">About</Link>
-                  <Link to="/faculty" className="text-gray-300 hover:bg-gray-600 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Faculty & Staff</Link>
-                  <Link to="/research" className="text-gray-300 hover:bg-gray-600 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Research</Link>
-                  <Link to="/studentoa" className="text-gray-300 hover:bg-gray-600 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Student OA</Link>
-                  
-                  <a href="#" className="text-gray-300 hover:bg-gray-600 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Student Achievers</a>
-                  <a href="#" className="text-gray-300 hover:bg-gray-600 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Community</a>
-                  <a href="#" className="text-gray-300 hover:bg-gray-600 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Alumni</a>
+                  <Link
+                    to="/"
+                    className="navbarlink"
+                  >
+                    Home
+                  </Link>
+                  <Link
+                    to="/about"
+                    className="navbarlink"
+                  >
+                    About
+                  </Link>
+                  <Link
+                    to="/faculty"
+                    className="navbarlink"
+                  >
+                    Faculty & Staff
+                  </Link>
+                  <Link
+                    to="/research"
+                    className="navbarlink"
+                  >
+                    Research
+                  </Link>
+                  <Link
+                    to="/studentoa"
+                    className="navbarlink"
+                  >
+                    Student O&A
+                  </Link>
+
+                  <a
+                    href="#"
+                    className="navbarlink"
+                  >
+                    Student Achievers
+                  </a>
+                  <a
+                    href="#"
+                    className="navbarlink"
+                  >
+                    Community
+                  </a>
+                  <a
+                    href="#"
+                    className="navbarlink"
+                  >
+                    Alumni
+                  </a>
                 </div>
               </div>
             </div>
@@ -76,14 +119,54 @@ const Navbar = () => {
         {/* Mobile menu, show/hide based on menu state. */}
         <div className="sm:hidden" id="mobile-menu">
           <div className="px-2 pt-2 pb-3 space-y-1">
-            <Link to="/" className="text-gray-300 hover:bg-gray-600 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Home</Link>
-            <Link to="/about" className="text-gray-300 hover:bg-gray-600 hover:text-white block px-3 py-2 rounded-md text-base font-medium">About</Link>
-            <Link to="/faculty" className="text-gray-300 hover:bg-gray-600 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Faculty & Staff</Link>
-            <Link to="/research" className="text-gray-300 hover:bg-gray-600 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Research</Link>
-            <a href="#" className="text-gray-300 hover:bg-gray-600 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Student OA</a>
-            <a href="#" className="text-gray-300 hover:bg-gray-600 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Student Achievers</a>
-            <a href="#" className="text-gray-300 hover:bg-gray-600 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Community</a>
-            <a href="#" className="text-gray-300 hover:bg-gray-600 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Alumni</a>
+            <Link
+              to="/"
+              className="navbarmobilelink"
+            >
+              Home
+            </Link>
+            <Link
+              to="/about"
+              className="navbarmobilelink"
+            >
+              About
+            </Link>
+            <Link
+              to="/faculty"
+              className="navbarmobilelink"
+            >
+              Faculty & Staff
+            </Link>
+            <Link
+              to="/research"
+              className="navbarmobilelink"
+            >
+              Research
+            </Link>
+            <a
+              href="#"
+              className="navbarmobilelink"
+            >
+              Student O&A
+            </a>
+            <a
+              href="#"
+              className="navbarmobilelink"
+            >
+              Student Achievers
+            </a>
+            <a
+              href="#"
+              className="navbarmobilelink"
+            >
+              Community
+            </a>
+            <a
+              href="#"
+              className="navbarmobilelink"
+            >
+              Alumni
+            </a>
           </div>
         </div>
       </nav>
