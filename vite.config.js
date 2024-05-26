bc034b76c71fd9925db347f2b858be062fa8f7f8
TIPKurtMacamay <133735418+TIPKurtMacamay@users.noreply.github.com>
2024-05-26 16:00:58 +0800

    Resolved merge conflict in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
   base: '/your-repository-name/' // Replace with your actual repository name
-=======
-  base: '/CPE_WEbsite_2024/' // Replace with your actual repository name
->>>>>>> 144e0f29147d468bef39067217209edf41974477
 })
