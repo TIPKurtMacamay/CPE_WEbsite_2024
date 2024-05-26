144e0f29147d468bef39067217209edf41974477
TIPKurtMacamay <133735418+TIPKurtMacamay@users.noreply.github.com>
2024-05-26 14:41:12 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/CPE_WEbsite_2024/' // Replace with your actual repository name
 })
